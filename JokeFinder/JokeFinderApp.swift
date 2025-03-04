21ca9faf2aa23a1c7a3d254f88ef68c84ae847bb
lcs-nhwang <sjnhwang@lcs.on.ca>
2025-03-04 14:07:44 -0500

    Initial Commit

---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by Nicholas Hwang on 4/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
